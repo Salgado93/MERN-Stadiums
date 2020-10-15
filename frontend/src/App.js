b6f1cd1f3da1a86a1daa84f8e1c2ad7bb638ede7
salgado93 <dsalgado93@outlook.com>
2020-10-15 12:25:29 -0600

    Storing Login token on the browser.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -19,13 +19,22 @@ const App = () => {
 
   const login = useCallback((uid,token) => {
     setToken(token);
+    localStorage.setItem('userData', JSON.stringify({userId: uid, token: token}));
     setUserId(uid);
   }, []);
 
   const logout = useCallback(() => {
     setToken(false);
-    setUserId(null)
+    setUserId(null);
+    localStorage.removeItem('userData');
   }, []);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('userData'));
+    if (storedData && storedData.token) {
+      login(storedData.userId, storedData.token)
+    }
+  }, [login]);
 
   let routes;
   if (token) {
